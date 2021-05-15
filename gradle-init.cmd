2fc1737afb53267c71bce3e7fc2f2aebf7d2fa38
denis-itskovich <denis.itskovich@dell.com>
2021-05-15 07:36:28 +0300

    [gradle] Updating wrapper

--- gradle-init.cmd
+++ gradle-init.cmd
@@ -1,1 +1,7 @@
-@copy %~dp0init\* %~dp0..\
+@echo off
+if "%1" == "all" (
+    copy %~dp0init\* %~dp0..\
+) else (
+    copy %~dp0init\gradlew %~dp0..\
+    copy %~dp0init\gradlew.bat %~dp0..\
+)
--- init/init.cmd
+++
@@ -1,8 +0,0 @@
-@echo off
-set CURRENT_DIR=%~dp0
-copy %CURRENT_DIR%\build.gradle %CURRENT_DIR%..\..
-copy %CURRENT_DIR%\gradle.properties %CURRENT_DIR%..\..
-copy %CURRENT_DIR%\.gitignore %CURRENT_DIR%..\..
-copy %CURRENT_DIR%\libs.properties %CURRENT_DIR%..\..
-copy %CURRENT_DIR%\gradlew %CURRENT_DIR%..\..
-copy %CURRENT_DIR%\gradlew.bat %CURRENT_DIR%..\..
